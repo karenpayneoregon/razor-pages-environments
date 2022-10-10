9da903811445a3a8f9297cf7462cb5bd60728702
Karen Payne <paynekaren@comcast.net>
2022-10-10 08:16:55 -0700

    Add project files.

---
+++ WebApplication1/Program.cs
@@ -0,0 +1,38 @@
+namespace WebApplication1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddRazorPages();
+
+            /*
+             * Karen note
+             * This is required when CSS Isolation is used other than Development environment
+             */
+            builder.WebHost.UseStaticWebAssets();
+            
+            var app = builder.Build();
+
+
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapRazorPages();
+
+            app.Run();
+        }
+    }
+}
